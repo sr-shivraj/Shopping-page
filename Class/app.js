cec076dbcbd06c1e90f4edaa075a0c4f8f741747
sr-shivraj <100534638+sr-shivraj@users.noreply.github.com>
2023-02-21 15:54:16 +0530

    Add files via upload

---
+++ Class/app.js
@@ -0,0 +1,22 @@
+let div=""
+let emp=[
+    {price:500, id:120, product_name:'Shirt', url:'Image/image-1.jpg'},
+    {price:500, id:121, product_name:'Pants', url:'Image/image-2.jpg'},
+    {price:200, id:122, product_name:'Tie', url:'Image/image-3.jpg'},
+    {price:300, id:123, product_name:'Shoes', url:'Image/image-4.jpg'},
+    {price:100, id:124, product_name:'Shocks', url:'Image/image-5.jpg'},
+    {price:600, id:125, product_name:'Perfume', url:'Image/image-6.jpg'},
+]
+emp.forEach(function(obj){
+
+div+=`
+<div class='col'>
+<div class='card'>
+<img src=${obj.url}>
+<h3>${obj.product_name}</h3>
+<p>${obj.price}</p>
+</div>
+</div>`
+
+})
+document.getElementById("row").innerHTML=div;
